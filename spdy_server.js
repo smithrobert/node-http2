c077c0ee9553a77bf067f3ca2ec43c34ef007373
Marwan Khalili <khalili@Marwans-MBP.lan>
2017-03-14 11:36:03 +0100

    Changed testfile

--- spdy_server.js
+++ spdy_server.js
@@ -2,35 +2,16 @@ const spdy = require('spdy');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('keys/privkey.pem'),
-  cert: fs.readFileSync('keys/fullchain.pem')
+  key: fs.readFileSync('../keys/privkey.pem'),
+  cert: fs.readFileSync('../keys/fullchain.pem')
 };
 
-var files = [];
-for (let i = 0; i <= 100; i++) {
-  files[i] = fs.readFileSync('./assets/' + i + "k");
-}
+var file = fs.readFileSync('../assets/100k')
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  var onRequest = function(request, response) {
-    for (let i = 0; i < files.length; i++) {
-      response.push('./' + i, {}, function(err, stream) {
-        if (err) return;
-        stream.end(files[i]);
-      });
-    }
+spdy.createServer(options, function(request, response) {
+  for (let i = 0; i < 100; i++) {
+    response.push('./' + i, {}, function(err, stream) {
+      stream.end(file);
+    });
   }
-} else {
-  var fileIndex = process.argv[2];
-  var repetition = process.argv[3];
-  var onRequest = function(request, response) {
-    for (let i = 0; i < repetition; i++) {
-      response.push('./' + i, {}, function(err, stream) {
-        if (err) return;
-        stream.end(files[fileIndex]);
-      });
-    }
-  }
-}
-
-spdy.createServer(options, onRequest).listen(443);
+}).listen(443);
